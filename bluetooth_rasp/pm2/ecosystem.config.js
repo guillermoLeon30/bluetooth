5cc9d20d6da6599ec95e64a1ecb49d72b9634967
guillermo <gleon2611@gmail.com>
2020-09-25 10:58:01 -0500

    mejora ecosystem pm2

--- bluetooth_rasp/pm2/ecosystem.config.js
+++ bluetooth_rasp/pm2/ecosystem.config.js
@@ -1,3 +1,16 @@
+/**
+ * Ajustar los valores de URL_API y PUERTO en el env, donde:
+ * 
+ * URL_API: es la direccion donde se enviaran los datos en formato json 
+ *          ejm: http://www.web.com/bluetooth, los datos son enviados por el metodo POST
+ *          con el siguiente formato { major: xxxx, minor: xxx }
+ * 
+ * PURTO: es el puerto donde apunta la api http://www.web.com:4000/bluetooth
+ * 
+ * Para activar la aplicacion usar el comando pm2 start ecosystem.config.js y luego pm2 save
+ * 
+ */
+
 module.exports = {
   apps: [{
     name: 'bluetooth',
@@ -7,8 +20,8 @@ module.exports = {
     out_file: '/home/pi/Documents/pm2/logs/bluetooth/out.log',
     time: true,
     env: {
-      'URL_API': '',
-      'PUERTO': 4000
+      'URL_API': '', // <------ cambiar aqui
+      'PUERTO': 4000 // <------ cambiar aqui
     }
   }]
 }
